abd64dbed11d5647fefd3ebe5e7258fb6936cd5d
Ethan Jucovy <ethan.jucovy@gmail.com>
2015-06-14 17:55:27 -0400

    whoops

--- coworker_tasks/models.py
+++ coworker_tasks/models.py
@@ -44,7 +44,7 @@ All columns apart from user_id and new_data_* will be ignored by the job code
 
             try:
                 f = CoreActionField.objects.using("ak").select_related("parent").filter(
-                    parent__page__name=row['originating_page_name'],
+                    parent__page__name=row['processing_page_name'],
                     parent__user__id=int(row['user_id']),
                     name="welcome_employer",
                     value=row['employer'])[0]
